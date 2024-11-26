96716f7b5ea05daa27ba6b296337d1479f35a157
Oludayo Adeoye <49393855+dbillion@users.noreply.github.com>
2024-11-26 19:02:18 +0000

    hey

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    // using raw sql
+    // $users = DB::select('select * from users ');
+    // dd($users);
+
+    //  quiery builder
+    $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+    // ('select * from users ');
+    dd($users);
 });
